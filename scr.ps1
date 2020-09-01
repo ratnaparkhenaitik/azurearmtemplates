e72d8b21e1c425ed37264d9a8f081e3d1d4b66e1
ratnaparkhenaitik <61557357+ratnaparkhenaitik@users.noreply.github.com>
2020-09-01 14:23:51 +0530

    Add files via upload

--- scr.ps1
+++ scr.ps1
@@ -1,11 +1,8 @@
-﻿
-
-$password= ConvertTo-SecureString ('$y$@dmin_2020') -AsPlainText -Force
+﻿$password= ConvertTo-SecureString ('$y$@dmin_2020') -AsPlainText -Force
 [int]$loop = 4
 for([int]$i=1;[int]$i -lt $loop;[int]$i++)
 {
 $user=New-LocalUser -AccountNeverExpires -Description 'my user' -Name mutti$i -Password $password 
 Add-LocalGroupMember -Group 'Administrators' -Member $user 
 }
-
 Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
